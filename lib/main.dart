19d7690695521b2b21bf0be884b05b9cc22fd559
Efaz <ishrakul236@gmail.com>
2024-01-08 02:19:34 +0600

    Project setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utilis/theme/theme.dart';
+import 'package:t_store/utils/theme/theme.dart';
+
+import 'app.dart';
 
 void main() {
+  // Todo: Add widgets Binding
+  // Todo: Init local storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
   runApp(const App());
 }
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme:TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
